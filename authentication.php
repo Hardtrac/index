4092dbf40d06e8310524170045f3dd9cc4639381
Hardtrac <162275055+Hardtrac@users.noreply.github.com>
2024-03-05 13:15:13 +0530

    authentication.php

--- authentication.php
+++ authentication.php
@@ -7,15 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Perform authentication
     if ($brandName === 'vinod' && $password === '1234') {
-        // Manipulate Excel sheet here
-        
-        // Provide link to modified file
-        $fileLink = 'https://docs.google.com/spreadsheets/d/1iazMmordxyAdktHm8RzSSp1CDsMLG8rR4q6avD9342E/edit#gid=2037114772';
-        echo '<script>window.location.href = "'.$fileLink.'";</script>';
+        // Redirect to Vinod's file
+        header('Location: https://docs.google.com/spreadsheets/d/1iazMmordxyAdktHm8RzSSp1CDsMLG8rR4q6avD9342E/edit#gid=2037114772');
         exit;
     } else {
         // Authentication failed
         echo '<script>alert("Invalid brand name or password. Please try again.");</script>';
+        // Redirect back to the login page
+        echo '<script>window.location.href = "index.html";</script>';
+        exit;
     }
 }
 ?>

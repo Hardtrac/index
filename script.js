97e3df4af71c3a3b9aef19bc5e8a175f51284f1e
Hardtrac <162275055+Hardtrac@users.noreply.github.com>
2024-03-07 11:06:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // You can add your login validation logic here
-    // For demonstration, I'm redirecting directly to the dashboard page
-    window.location.href = 'dashboard.html';
-});
